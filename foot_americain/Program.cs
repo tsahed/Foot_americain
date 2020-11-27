6842828848034a3a6076cccf201cacca048adf3a
thalia sahed <thalia.sahed@saintmichelannecy.fr>
2020-11-27 12:09:19 +0100

    DAO

---
+++ foot_americain/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace foot_americain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
